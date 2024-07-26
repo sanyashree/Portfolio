99ce82088e4eb35cedc8b8092a750545b431b06c
sanyashree <149993907+sanyashree@users.noreply.github.com>
2024-07-26 14:22:43 +0530

    Add files via upload

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  return (
+    <>
+      <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Languages</h2>
+        <div className={styles.content}>
+          <div className={styles.history}>
+            {history.map((skill, id) => (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.container} id="experience">
+        <h2 className={styles.title}>Skills</h2>
+        <div className={styles.content}>
+          <div className={styles.skills}>
+            {skills.map((skill, id) => (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Sanya</h1>
+        <p className={styles.description}>
+        I have a strong foundation in programming, problem-solving, and web development. 
+        Eager to leverage skills in DSA and competitive programming to contribute 
+        to innovative projects.
+        </p>
+        <a href="mailto:sanyashreeofficial@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
